918fe2e18091cc064a9e6d54a68e9f74291a3167
VrajLalwala22 <vrajlalwalawork@gmail.com>
2025-03-20 02:26:31 +0530

    frontend update

--- RentPeCar_Frontend/src/Admin/Components/SidebarData.js
+++ RentPeCar_Frontend/src/Admin/Components/SidebarData.js
@@ -6,7 +6,7 @@ import * as IoIcons from 'react-icons/io';
 
 export const SidebarData  = [
   {
-    title: 'PG Building',
+    title: 'PG Buildings',
     path: '/PgBuildings',
     icon: <AiIcons.AiFillHome />,
     cName: 'nav-text'
